4a2d1a8a9edaf7f938ecd833c3111537b08d1690
tanhao <tanhao@wondersgroup.com>
2021-02-25 13:29:40 +0800

    自定义协议头

---
+++ tcp-protocol/src/main/java/com/github/tanhao1410/thesis/protocol/TCPProtocolConstant.java
@@ -0,0 +1,10 @@
+package com.github.tanhao1410.thesis.protocol;
+
+/**
+ * @author tanhao
+ * @date 2021/02/25 11:08
+ */
+public class TCPProtocolConstant {
+
+    public static final int HEAD = 19225338;
+}
